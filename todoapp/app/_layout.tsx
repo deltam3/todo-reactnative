dfbcc3c92b2e0165dfe3e6fe87c28a61e954b9ef
deltam3 <practicect@proton.me>
2025-04-03 15:40:15 +0900

    layout edit

--- todoapp/app/_layout.tsx
+++ todoapp/app/_layout.tsx
@@ -4,34 +4,18 @@ import {
   ThemeProvider,
 } from "@react-navigation/native";
 import { useFonts } from "expo-font";
-// import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
-// import { useEffect } from "react";
+import { useEffect } from "react";
 // import "react-native-reanimated";
 
 import { useColorScheme } from "@/hooks/useColorScheme";
 
-// // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 // import { Link, Redirect, useRouter } from "expo-router";
 
 // export default function RootLayout() {
-//   const colorScheme = useColorScheme();
-//   const [loaded] = useFonts({
-//     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-//   });
-
-//   useEffect(() => {
-//     if (loaded) {
-//       SplashScreen.hideAsync();
-//     }
-//   }, [loaded]);
-
-//   // if (!loaded) {
-//   //   return null;
-//   // }
 
 //   // return <Redirect href="/onboarding" />;
 
@@ -51,6 +35,20 @@ import { Stack } from "expo-router";
 import { SafeAreaView, View, Text, StyleSheet } from "react-native";
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <Stack screenOptions={{ headerShown: false }}>
