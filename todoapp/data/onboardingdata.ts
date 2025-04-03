c45efd24469465e0097a5bdca678a2b191749c01
deltam3 <practicect@proton.me>
2025-04-03 11:36:02 +0900

    onboarding data

---
+++ todoapp/data/onboardingdata.ts
@@ -0,0 +1,20 @@
+export default [
+  {
+    id: 1,
+    img: require("../assets/images/step1.jpg"),
+    title: "혁명적인 Todo 리스트 어플!",
+    description: "기존의 Todo 어플과는 다른 어플!",
+  },
+  {
+    id: 2,
+    img: require("../assets/images/step2.jpg"),
+    title: "완벽한 보안",
+    description: "철저한 보안 시스템으로 여러분의 정보를 보호합니다.",
+  },
+  {
+    id: 3,
+    img: require("../assets/images/step3.jpg"),
+    title: "세계 1위 Todo 어플",
+    description: "비교가 안 되는 어플",
+  },
+];
