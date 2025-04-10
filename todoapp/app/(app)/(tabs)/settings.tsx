ef9299fae9b29f316987f5a43f89fadf22e6778f
deltam3 <practicect@proton.me>
2025-04-10 22:55:25 +0900

    settings screen isAuthenticated protection

--- todoapp/app/(app)/(tabs)/settings.tsx
+++ todoapp/app/(app)/(tabs)/settings.tsx
@@ -1,6 +1,13 @@
+import useCheckLoginStatus from "@/hooks/useCheckLoginStatus";
+import { Redirect } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Settings() {
+  const isAuthenticated = useCheckLoginStatus();
+
+  if (!isAuthenticated) {
+    return <Redirect href="/(auth)/loginScreen" />;
+  }
   return (
     <View>
       <Text>Settings</Text>
